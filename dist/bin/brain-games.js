2327bca00e6bcd58f8607e50e9d1a8b798e3c540
k4r1s0n <alex8under8@gmail.com>
2018-08-21 04:48:08 -0700

    final3

--- dist/bin/brain-games.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-var _ = require('..');
-
-(0, _.welcomeMessage)();
-(0, _.sayName)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFFQTs7QUFFQTtBQUNBIiwiZmlsZSI6ImJyYWluLWdhbWVzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG5cbmltcG9ydCB7IHdlbGNvbWVNZXNzYWdlLCBzYXlOYW1lIH0gZnJvbSAnLi4nO1xuXG53ZWxjb21lTWVzc2FnZSgpO1xuc2F5TmFtZSgpOyJdfQ==
