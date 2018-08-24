02a798949dcaf03260018acc934cf79b6bef66db
k4r1s0n <alex8under8@gmail.com>
2018-08-24 04:18:37 -0700

    final6

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -5,15 +5,15 @@ const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min
 const calc = () => {
   const firstNum = getRandomNum(1, 100);
   const secondNum = getRandomNum(1, 100);
-  const numForExpression = getRandomNum(1, 100);
+  const numToPickExpression = getRandomNum(1, 100);
   let question;
   let correctAnswer;
-  switch (numForExpression) {
-    case numForExpression < 33: //  33 - expression probability
+  switch (numToPickExpression) {
+    case numToPickExpression < 33: //  33 - expression probability
       question = `${firstNum} + ${secondNum}`;
       correctAnswer = firstNum + secondNum;
       break;
-    case numForExpression < 66:
+    case numToPickExpression < 66:
       question = `${firstNum} - ${secondNum}`;
       correctAnswer = firstNum - secondNum;
       break;
